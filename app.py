511da684c13a4a9860640090259eb1b2761fcf02
baamosk <bunth667@gmail.com>
2018-10-20 15:01:30 -0400

    not creating database

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+import psycopg2
+from config import url
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "NPOST.ME"
+app.config['SQLACHEMY_DATABASE_URI'] = url
+db = SQLAlchemy(app)
+
+class NPO(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'{self.id} {self.name}'
 
 @app.route('/')
 def index():
