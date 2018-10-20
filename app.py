d2b8103831f51924247bd02fe6aff2218bdb9f7b
F.J.H. Faneus <ffaneus@icloud.com>
2018-10-20 14:59:48 -0400

    Code for html and app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
-
+from forms import Npo_form
 app = Flask(__name__)
-
-@app.route('/')
+app.config['DEBUG']=True
+app.config['SECRET_KEY']="True"
+@app.route('/',methods=["POST","GET"])
 def index():
-    return render_template('index.html')
+    form=Npo_form()
+    return render_template('index.html',form=form)
 
 if __name__ == "__main__":
     app.run()
