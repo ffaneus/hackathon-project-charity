a6985dd5a84baeac49ece50cb8d70fdc4aa61f24
pyronitrex <34744152+pyronitrex@users.noreply.github.com>
2018-10-20 11:38:18 -0400

    Classes

---
+++ NPO.py
@@ -0,0 +1,14 @@
+class NPO():
+    def __init__(self, website, name, email, number, donor, volunteer, image, volunteer_num):
+        self.website = website
+        self.name = name
+        self.email = email
+        self.number = number
+        self.donor = donor
+        self.volunteer = volunteer
+        self.image = image
+        self.volunteer_num = volunteer_num
+    def post(self):
+        """Post using html or flask"""
+    def delete(self):
+        """Delete using html or flask"""
