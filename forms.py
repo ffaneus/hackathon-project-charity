aff429932cc7b63bb32318feeee7cdda552715b9
F.J.H. Faneus <ffaneus@icloud.com>
2018-10-20 17:40:30 -0400

    more commits

--- forms.py
+++ forms.py
@@ -7,19 +7,19 @@ from wtforms.validators import InputRequired, Length, Email, URL
         raise ValueError('Please enter a valid phone number')'''
 
 class Npo_form(FlaskForm):
-    name = StringField("What is the name of your organization?", validators=[InputRequired(message="Organization name is required!"), Length(min=100, max=1000)])
-    phone_number = StringField("Please enter a phone number where your organization can be contacted:", validators=[InputRequired()])
-    email = StringField("Please enter an email address where your organization can be contacted:", validators=[InputRequired(message="Email address is required"), Email()])
-    web_site = StringField("Please enter your organization's url:", validators=[URL(require_tld=True, message="That is not a valid URL.")])
-    description = StringField("Tell us a little about your organization:", validators=[InputRequired(message="Please tell us about your organization."), Length(min=100,max=2000)])
-    seeking = StringField("Tell us a little bit about what your organization is looking for:", validators=[InputRequired("Please describe what you are looking for."), Length(min=100,max=2000)])
-    Contributors = SelectField("Please select which type of contribution your organization is looking for ... ", choices=[("Donors","Select this option if you are seeking contributions of material goods."),("Volunteers","Select this option if you are seeking contributions of time."),("Donors and Volunteers","Select this option if you are seeking both contributions of material and time.")])
+    name = StringField("Organization Name:", validators=[InputRequired(message="Organization name is required!"), Length(min=100, max=1000)])
+    phone_number = StringField("Phone Number:", validators=[InputRequired()])
+    email = StringField("Email Address:", validators=[InputRequired(message="Email address is required"), Email()])
+    web_site = StringField("Organization URL:", validators=[URL(require_tld=True, message="That is not a valid URL.")])
+    description = StringField("Description of Organization:", validators=[InputRequired(message="Please tell us about your organization."), Length(min=100,max=2000)])
+    seeking = StringField("Description of Resources Needed:", validators=[InputRequired("Please describe what you are looking for."), Length(min=100,max=2000)])
+    Contributors = SelectField("Type of Contribution Organization seeking ... ", choices=[("Donors","Select this option if you are seeking contributions of material goods."),("Volunteers","Select this option if you are seeking contributions of time."),("Donors and Volunteers","Select this option if you are seeking both contributions of material and time.")])
     submit=SubmitField("Submit")
-
+    
 '''class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     email = StringField("School Email", validators=[DataRequired()])
-    submit = SubmitField("Login")
+    submit = SubmitField()
 
 #FORM to pass data
 class QuestionForm(FlaskForm):
