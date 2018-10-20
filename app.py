7f6eab66ed04f9d6810270384ebe7e9e2a974cd3
baamosk <bunth667@gmail.com>
2018-10-20 11:31:36 -0400

    donor page linking to npos

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/donor')
+def donor():
+    return render_template('donor.html')
+
+
 if __name__ == "__main__":
     app.run()
